9bf9871cb8f0bc81bda196dfc94f095c70a80b6b
xamgooh <151753586+xamgooh@users.noreply.github.com>
2025-08-28 13:20:09 +0200

    Update class-comparison-shortcode.php

--- includes/class-comparison-shortcode.php
+++ includes/class-comparison-shortcode.php
@@ -28,7 +28,6 @@ class ComparisonShortcode
 	public function __construct()
 	{
 		$this->comparison = 'com_comporison';
-		add_shortcode('Comparison', array($this, 'custom_shortcode'));
 		add_shortcode('Comparison_v2', array($this, 'custom_shortcode_v2'));
 		$this->comparion_list_html = new ComparisonHtml();
 	}
@@ -36,7 +35,6 @@ class ComparisonShortcode
 	// get comparison posts list
 	public function get_comarison_posts($category, $max, $list_id = null)
 	{
-
 		$max  = ($max === '0' || (int)$max >= 1 || (int)$max >= -1) ? (int)$max : 15;
 
 		if ($max < 1 && $max != -1) {
@@ -57,8 +55,6 @@ class ComparisonShortcode
 			foreach ($comparison_list_metabox['brand_in_list'] as $key => $value) {
                 array_push($posts_ids, [ "brand_id" => $value['select_post'], "brand_other_link" => $value['brand_other_link']]);
             }
-
-			//asort($posts_ids);
 			
 			if ($comparison_list_metabox) {
 				foreach ($posts_ids as $key => $value) {
@@ -83,97 +79,7 @@ class ComparisonShortcode
 		return new WP_Query($args);
 	}
 
-
-
-	// Add Shortcode
-	public function custom_shortcode($atts)
-	{
-		$html = '';
-		$random_id = mt_rand(100, 500);
-		// Attributes
-		$attributes  = shortcode_atts(
-			array(
-				'max' 			=> 15,
-				'category' 		=> '',
-				'post_status' 	=> 'publish',
-				'filter'		=> false,
-				'load_more'		=> false
-			),
-			$this->normalize_empty_atts($atts),
-			'Comparison'
-		);
-
-		$category = $this->transform_string_to_array($attributes['category']);
-
-		$filter = $attributes['filter'];
-
-		$html = '<section class="com-comparison-plugin">';
-		if (is_array($category) && count($category) > 1 && $filter) {
-
-			$html .= '<div class="tabell-dropdown">
-					<button class="com__btn" type="button" aria-expanded="true">
-					<span class="com__icon--menu" aria-hidden="true"></span>' . __(get_option('comporisons_sorting_label'), "comporisons") . '
-					</button>
-				</div>
-				<ul class="category__navpills isotop__grid hidden">';
-
-			foreach ($category as $key => $value) {
-				$args = array(
-					'taxonomy' => 'com_category',
-					'orderby' => 'name',
-					'slug' => mb_strtolower(trim($value)),
-					'order'   => 'ASC'
-				);
-				$cats = get_categories($args);
-
-				$html .= '<li role="presentation" class="isotop__grid--list ' . ($key === array_key_first($category) ? 'active' : '') . '">
-					<a data-key="' . $random_id . '_' . str_replace(' ', '-', mb_strtolower(trim($value))) . '" class="isotop__grid--item" href="#" rel="nofollow  noopener">';
-				if (is_array($cats)  && isset($cats[0])) {
-					$cat_icon_url = get_option('weekend-series_' . $cats[0]->term_id);
-					if (is_array($cat_icon_url) && array_key_exists('image', $cat_icon_url) && !empty($cat_icon_url['image'])) {
-						$html .= '<img style="width: 30px; height: 30px" alt="icon" class="com_comparision_icon"' . ($cat_icon_url['image'] ? 'src="' . $cat_icon_url['image'] . '"' : '') . '>';
-					} else {
-						$html .= '<span style="width: 30px; height: 30px"></span>';
-					}
-				}
-				$html .= '<span>' . $value . '</span></a></li>';
-			}
-
-			$html .= '</ul>';
-
-			foreach ($category as $key => $value) {
-				$value = str_replace(' ', '-', mb_strtolower(trim($value)));
-				$wp_query = $this->get_comarison_posts([0 => $value], $attributes['max']);
-
-				$hidden = !($key === array_key_first($category)) ? " hidden" : "";
-				$html .= '<div class="com_comarison__list--container ' . $random_id . '_' . $value . $hidden . '">';
-				$html .= $this->comparion_list_html->get_list_html($wp_query);
-				if ($wp_query->found_posts > $attributes['max'] && $attributes['load_more']) {
-					$html .= '<button data-restUrl="' . site_url() . '" data-list="' . $value . '" data-limit="' . $attributes['max'] . '" class="com_btn--loadmore">' . get_option('comporisons_filter_label') . '</button>';
-				}
-
-				$html .= '</div>';
-			}
-		} else {
-			$wp_query = $this->get_comarison_posts($category, $attributes['max']);
-			$html .= '<div class="com_comarison__list--container ' . $random_id . '_' . $random_id . '">';
-			$html .= $this->comparion_list_html->get_list_html($wp_query, $attributes['max']);
-			if ($wp_query->found_posts > $attributes['max'] && $attributes['load_more']) {
-				if (is_array($category)) {
-					$html .= '<button data-restUrl="' . site_url() . '" data-list="' . str_replace(' ', '-', mb_strtolower(trim($category[0]))) . '" data-limit="' . $attributes['max'] . '" class="com_btn--loadmore">' . get_option('comporisons_filter_label') . '</button>';
-				} else {
-					$html .= '<button data-restUrl="' . site_url() . '" class="com_btn--loadmore">' . get_option('comporisons_filter_label') . '</button>';
-				}
-			}
-			$html .= '</div>';
-		}
-
-		$html .= '</section><div class="com-comparison__clearfix"></div>';
-
-		return $html;
-	}
-
-	// Add V2 Shortcode
+	//  V2 Shortcode
 	public function custom_shortcode_v2($atts)
 	{
 		$html = '';
@@ -263,9 +169,6 @@ class ComparisonShortcode
 
 		return $html;
 	}
-
-
-
 
 	// transform string to array
 	public function transform_string_to_array($str)
